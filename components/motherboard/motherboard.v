6fb477c1adb59f3011e8cb64a89db0d37051ccfb
Patrick Brown <pmbrown@andrew.cmu.edu>
2014-10-20 19:31:13 -0400

    added the beginnings of the motherboard

---
+++ components/motherboard/motherboard.v
@@ -0,0 +1,12 @@
+/*
+ * motherboard.v
+ * Verilog description of the main motherboard of the IBM PC
+ */
+module motherboard(
+		   input USER_CLK, // user clock at 100 MHz
+		   
+		   );
+
+   // Internal signals
+
+endmodule // motherboard
