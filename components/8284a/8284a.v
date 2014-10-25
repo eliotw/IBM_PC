e91e4bfe364c628c068f701fa4a9cd4a08c584a3
Eliot Wong <eliotw@cmu.edu>
2014-10-24 21:49:40 -0400

    Updated fpga_clk trigger and added basic logic

--- components/8284a/8284a.v
+++ components/8284a/8284a.v
@@ -1,5 +1,5 @@
-
 module 8284a(
+        fpga_clk,
         rdy1,
         aen1,
         ready,
@@ -10,13 +10,24 @@ module 8284a(
         vclk
 );
 
-input rdy1, aen1;
+input fpga_clk, rdy1, aen1;
 output ready, clk, reset, osc, pclk, vclk;
 
 reg [5:0] counter;
 reg [2:0] counter2;
 
-always @(posedge clk) begin
+wire a1, a2, o1, q1;
+
+and and1(a1, rdy1, ~aen1) 
+assign a2 = 1'b0;
+or or1(o1, a1, a2);
+dff dff1(o1, clk, q1);
+and and2(a3, o1, q1);
+dff dff2(a3, clk, ready);
+
+
+
+always @(posedge fpga_clk) begin
     if(counter == 6'd42)
         counter <= 0;
     else
@@ -54,4 +65,22 @@ always @(posedge clk) begin
         vlk <= 1'b0;
     end
 
+
+
 endmodule // 8284a
+
+module dff(
+    data,
+    clk,
+    q
+);
+
+input data, clk;
+output q;
+
+reg q;
+
+always @(posedge clk)
+    q <= data;
+
+endmodule // dff
