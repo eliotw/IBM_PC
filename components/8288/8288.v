31474371f014c200725788150b3cf6132f02ad85
Patrick Brown <pmbrown@andrew.cmu.edu>
2014-10-25 18:32:10 -0400

    worked on stuff

--- components/8288/8288.v
+++ components/8288/8288.v
@@ -1,26 +1,75 @@
+/*
+ * intel8288:
+ * The bus controller for the IBM PC
+ */
+module intel8288(
+		 input [2:0] s_n,
+		 input clk,
+		 input aen_n,
+		 input cen, // output enable
+		 input iob, // set equal to 0
+		 output mrdc_n,
+		 output mwtc_n, // not connected
+		 output amwc_n,
+		 output iorc_n,
+		 output iowc_n, // not connected
+		 output aiowc_n,
+		 output inta_n,
+		 output dtr,
+		 output den,
+		 output mce, // not connected
+		 output ale
+		 );
 
-module 8288(
-	    s,
-	    inta_n,
-	    clk,
-	    aen_n,
-	    cen,
-	    ale,
-	    den,
-	    dtr,
-	    pden_n,
-	    aiowc_n,
-	    amwc_n,
-	    iorc_n,
-	    mrdc_n
-	    );
+   // Parameters
+   parameter [1:0]
+		s0 = 2'b00,
+		s1 = 2'b01,
+		s2 = 2'b10,
+		s3 = 2'b11;
+   
+   // Registers
+   reg [1:0] 		state, nextstate; // States
+   reg 			mrdc_n_s, amwc_n_s, iorc_n_s, aiowc_n_s, inta_n_s;
+   reg 			dtr_s, den_s, ale_s;
+   
+   // Wires
 
-   input [2:0] s;
-   input       inta_n;
-   input       clk;
-   input       aen_n, cen;
-   output      ale, oen, dtr, aiowc_n, amwc_n, iorc_n, mrdc_n;
+   // Assign unconnected signals
+   assign mwtc_n = 1'b1;
+   assign iowc_n = 1'b1;
+   assign mce = 1'b1;
 
-endmodule // 8288
+   // Assign connected signals
+   assign mrdc_n = (cen) ? mrdc_n_s : 1'b1;
+   assign amwc_n = (cen) ? amwc_n_s : 1'b1;
+   assign iorc_n = (cen) ? iorc_n_s : 1'b1;
+   assign aiowc_n = (cen) ? aiowc_n_s : 1'b1;
+   assign inta_n = (cen) ? inta_n_s : 1'b1;
+   assign dtr = (cen) ? dtr_s : 1'b1; 
+   assign den = (cen) ? den_s : 1'b0;
+   assign ale = (cen) ? ale_s : 1'b0;
+
+   // Latching of relevant input
+   always @(posedge clk) begin
+      if(state == s1) begin
+	 mrdc_n_s <= ~((s == 3'b100) | (s == 3'b101));
+	 amwc_n_s <= ~(s == 3'b110);
+	 iorc_n_s <= ~(s == 3'b001);
+	 aiowc_n_s <= ~(s == 3'b010);
+	 inta_n_s <= ~(s == 3'b000);
+      end
+      else begin
+	 mrdc_n_s <= mrdc_n_s;
+	 amwc_n_s <= amwc_n_s;
+	 iorc_n_s <= iorc_n_s;
+	 aiowc_n_s <= aiowc_n_s;
+	 inta_n_s <= inta_n_s;
+      end // else: !if(state == s1)
+   end
+   
+   
+endmodule // intel8288
+
 
    
