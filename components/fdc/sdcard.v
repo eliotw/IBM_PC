136608870b444b0c2647b52b3260d1d61b7fdd4b
Patrick Brown <pmbrown@andrew.cmu.edu>
2014-10-31 10:38:11 -0400

    adding some more floppy stuff

---
+++ components/fdc/sdcard.v
@@ -0,0 +1,19 @@
+/*
+ * sdcard:
+ * Verilog description of module that mimics the SD card reader in the ao486
+ * floppy disk file
+ */
+
+module sdcard(
+	      // inputs go here
+	      );
+
+   // Wires
+
+   // Registers
+
+   // Assignments
+
+   // Block RAM
+
+endmodule // sdcard
