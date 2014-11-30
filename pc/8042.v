2329ab3cd807ef008561c266da8b0699a9cab159
Patrick Brown <pmbrown@andrew.cmu.edu>
2014-11-30 17:19:05 -0500

    we now have a multiplayer game

--- pc/8042.v
+++ pc/8042.v
@@ -594,7 +594,7 @@ module keyload(
 		act6 = 8'b1000_0000;
 	
 	// Terminal Count for Counter
-	parameter [15:0] tc = 16'd14915; // 14918 lines in file
+	parameter [15:0] tc = 16'd14899; // 14902 lines in file
 	parameter [15:0] endcount = 16'h4000; // 0x4000 is FAST ENOUGH!
 	
 	// Counter Register
